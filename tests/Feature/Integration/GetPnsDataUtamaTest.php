39fefb3442ec29491d48e745d387202ff32440f1
rian <rianrieski@yahoo.com>
2023-09-14 08:18:18 +0700

    wip test

--- tests/Feature/Integration/GetPnsDataUtamaTest.php
+++ tests/Feature/Integration/GetPnsDataUtamaTest.php
@@ -1,14 +1,33 @@
 <?php
 
 use App\Integration\Siasn\Connector\SiasnSimpegConnector;
+use App\Integration\Siasn\Request\CreateApimwsToken;
+use App\Integration\Siasn\Request\CreateSiasnTokenRequest;
 use App\Integration\Siasn\Request\GetPnsDataUtama;
 use App\Jobs\GetSiasnTokenJob;
+use Saloon\Http\Faking\MockResponse;
 
-it('get siasn pns data utama', function () {
-    GetSiasnTokenJob::dispatch();
-    $connector = new SiasnSimpegConnector();
+beforeEach(function () {
+   \Saloon\Laravel\Facades\Saloon::fake([
+       GetPnsDataUtama::class => MockResponse::fixture('pns/data-utama'),
+       CreateSiasnTokenRequest::class => MockResponse::fixture('token/siasn-token'),
+       CreateApimwsToken::class => MockResponse::fixture('token/apimws-token')
+   ]);
+});
 
-    $response = $connector->send(new GetPnsDataUtama(198808042015021001));
+it('can fetch siasn pns data utama', function () {
+    GetSiasnTokenJob::dispatchSync();
 
-    dd($response->json());
+    $connector = new SiasnSimpegConnector;
+
+//    $response = $connector->sendAndRetry(new GetPnsDataUtama(198808042015021001), 2, 10, function ($exception, PendingRequest $pendingRequest) {
+//        dd($exception);
+//    });
+
+
+//    $service = new SiasnService();
+//
+//    $response = $service->fetchPnsDataUtama(198808042015021001);
+
+    dd($response);
 });
