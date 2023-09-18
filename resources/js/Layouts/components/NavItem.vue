a41a9b901b7ead8fb5cbec9e428246efa31b0948
TEGUH RIYONO <teguh.riyono.ri@gmail.com>
2023-09-18 09:05:57 +0700

    Update Hak Akses User

--- resources/js/Layouts/components/NavItem.vue
+++ resources/js/Layouts/components/NavItem.vue
@@ -1,13 +1,33 @@
 <script setup>
 import useRouteStore from "@/Stores/RouteStore.js";
+import { usePage } from '@inertiajs/vue3';
+import { computed } from "vue";
+const page = usePage()
 
 defineProps(["item"]);
 
 const routes = useRouteStore();
+
+const akses = computed(() => page.props.auth.akses)
+
+function checkPermission(strPermission, permission) {
+    // console.log('check permission:' + strPermission);
+    // console.log(akses);
+
+    if (permission[strPermission]){
+        return true;
+    }else{
+        return false;
+    }
+
+}
+
 </script>
 
 <template>
-    <li v-if="routes.isHasAccess(item) && item.children">
+
+    <li v-if="routes.isHasAccess(item,akses) && item.children">
+
         <details :open="false">
             <summary>
                 <component :is="item.icon" class="mr-2 w-6" />
@@ -18,10 +38,12 @@ const routes = useRouteStore();
                 v-for="row in item.children"
                 :key="row.label"
                 :item="row"
+                :v-if="checkPermission(row.label,akses)"
             />
         </details>
+
     </li>
-    <li v-else-if="routes.isHasAccess(item)">
+    <li v-else-if="routes.isHasAccess(item, akses)">
         <Link :href="item.href">
             <component :is="item.icon" class="mr-2 w-6" />
             <span class="font-semibold">{{ item.label }}</span>
--- resources/js/Stores/RouteStore.js
+++ resources/js/Stores/RouteStore.js
@@ -7,6 +7,12 @@ import {
     MapPinIcon,
     CubeTransparentIcon
 } from "@heroicons/vue/24/outline/index.js";
+import { usePage } from '@inertiajs/vue3';
+import { computed } from "vue";
+const page = usePage()
+
+const akses = computed(() => page.props.auth.akses)
+
 
 const useRouteStore = defineStore("route-store", () => {
     const list = [
@@ -15,22 +21,26 @@ const useRouteStore = defineStore("route-store", () => {
             label: "Dashboard",
             href: route("dashboard"),
             name: "dashboard",
+            permission: "dashboard",
         },
         {
             icon: BriefcaseIcon,
             label: "Unit Kerja",
             href: "/",
             name: "work-unit.*",
+            permission: "unit_kerja",
             children: [
                 {
                     icon: ClipboardIcon,
                     label: "Test",
-                    href:"#"
+                    href:"#",
+                    permission: "test",
                 },
                 {
                     icon: CubeTransparentIcon,
                     label: "Hirarki Unit Kerja",
-                    href:route('hirarki-unit-kerja.index')
+                    href:route('hirarki-unit-kerja.index'),
+                    permission: "hirarki_unit_kerja_list",
                 },
             ],
         },
@@ -39,41 +49,49 @@ const useRouteStore = defineStore("route-store", () => {
             label: "Pegawai",
             href: "/",
             name: "work-unit.*",
+            permission: "pegawai",
             children: [
                 {
                     icon: MapPinIcon,
                     label: "Alamat Pegawai",
-                    href: route("alamat.index")
+                    href: route("alamat.index"),
+                    permission: "alamat_pegawai_list",
                 },
                 {
                     icon: MapPinIcon,
                     label: "Riwayat Diklat",
-                    href: route("riwayat-diklat.index")
+                    href: route("riwayat-diklat.index"),
+                    permission: "riwayat_diklat_list",
                 },
                 {
                     icon: MapPinIcon,
                     label: "Tmt Gaji",
-                    href: route("tmt-gaji.index")
+                    href: route("tmt-gaji.index"),
+                    permission: "tmt_gaji_list",
                 },
                 {
                     icon: MapPinIcon,
                     label: "Riwayat Pendidikan",
-                    href: route("riwayat-pendidikan.index")
+                    href: route("riwayat-pendidikan.index"),
+                    permission: "riwayat_pendidikan_list",
                 },
                 {
                     icon: MapPinIcon,
                     label: "Anak",
-                    href: route("anak.index")
+                    href: route("anak.index"),
+                    permission: "anak_list",
                 },
                 {
                     icon: MapPinIcon,
                     label: "Suami Istri",
-                    href: route("suami-istri.index")
+                    href: route("suami-istri.index"),
+                    permission: "suami_istri_list",
                 },
                 {
                     icon: MapPinIcon,
                     label: "Saldo Cuti",
-                    href: route("saldo-cuti.index")
+                    href: route("saldo-cuti.index"),
+                    permission: "saldo_cuti_list",
                 },
             ],
         },
@@ -82,21 +100,25 @@ const useRouteStore = defineStore("route-store", () => {
             label: "Master",
             href: "/",
             name: "work-unit.*",
+            permission: "master",
             children: [
                 {
                     icon: ClipboardIcon,
                     label: "Tunjangan Kinerja",
-                    href: route("tukin.index")
+                    href: route("tukin.index"),
+                    permission: "tukin_list",
                 },
                 {
                     icon: ClipboardIcon,
                     label: "Uang Makan",
-                    href: route("umak.index")
+                    href: route("umak.index"),
+                    permission: "uang_makan_list",
                 },
                 {
                     icon: ClipboardIcon,
                     label: "Hari Libur",
-                    href: route("hari-libur.index")
+                    href: route("hari-libur.index"),
+                    permission: "hari_libur_list",
                 },
             ],
         },
@@ -105,31 +127,50 @@ const useRouteStore = defineStore("route-store", () => {
             label: "Pengaturan",
             href: "/",
             name: "work-unit.*",
+            permission: "pengaturan",
             children: [
                 {
                     icon: ClipboardIcon,
                     label: "Role",
-                    href: route("role.index")
+                    href: route("role.index"),
+                    permission: "role_list",
                 },
                 {
                     icon: ClipboardIcon,
-                    label: "Users",
-                    href: route("user.index")
+                    label: "Pengguna",
+                    href: route("user.index"),
+                    permission: "pengguna_list",
                 },
                 {
                     icon: ClipboardIcon,
                     label: "Hak Akses",
-                    href: route("permission.index")
+                    href: route("permission.index"),
+                    permission: "hak_akses_list",
                 },
             ],
         },
     ];
 
-    const isHasAccess = (item) => {
-        if (!item.hasOwnProperty("showIf")) {
+    function checkPermission(strPermission, permission) {
+        console.log('check permission:' + strPermission);
+        console.log(permission[strPermission]);
+
+        if (permission[strPermission]){
             return true;
+        }else{
+            return false;
         }
 
+    }
+
+    const isHasAccess = (item,permission) => {
+        if (!item.hasOwnProperty("showIf")) {
+            if (checkPermission(item.permission,permission)){
+                return true;
+            }else{
+                return false;
+            }
+        }
         return item.showIf();
     };
 
